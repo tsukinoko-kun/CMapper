bce28774c0e585d3e0aae3da590f9fc5b7065b81
Frank Mayer <frank.mayer19072001@outlook.de>
2020-11-02 15:30:40 +0100

    Umlaute

--- src/script/lib.ts
+++ src/script/lib.ts
@@ -1,6 +1,6 @@
 function remSpCh(str: string) {
-  return str.replace(/[^a-zA-Z0-9 ]/g, "");
+  return str.replace(/[^a-zA-Z0-9öäüÖÄÜß ]/g, "");
 }
 function remSpChCard(str: string) {
-  return str.replace(/[^a-zA-Z0-9\*\. ]/g, "");
+  return str.replace(/[^a-zA-Z0-9\söäüÖÄÜß\*\. ]/g, "");
 }
